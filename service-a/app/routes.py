68a473cda8a143509190e0aca3ea4e1682dbc0c9
yehuda linker <yehuda.linker@gmail.com>
2026-01-14 10:30:43 +0200

    its work!

--- service-a/app/routes.py
+++ service-a/app/routes.py
@@ -9,7 +9,7 @@ router = APIRouter()
 def post(input1:CoordinatIp):
     respons = services.ip_to_json(input1.ip)
     json_to_send = services.Clean_json(respons)
-    status = services.connectinga_servers(json_to_send)
+    status = services.connecting_servers(json_to_send)
     return json_to_send
 
 
